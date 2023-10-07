9657f9497dd9ddd914b2c6566b2fb1065819a611
Giovanni Martins <giovanni.martins@stone.com.br>
2023-10-07 20:33:09 -0300

    ✨ Adf validation in register fields

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -3,9 +3,11 @@ import { IonButton, IonIcon, IonInput, IonTitle } from '@ionic/react';
 import { sparkles, arrowBack, mail, paperPlane, arrowForward, add } from 'ionicons/icons';
 import './Login.css';
 import { useMaskito } from '@maskito/react';
+import { validateField } from '@dakasa/mfe-utils';
+import axios from 'axios';
 
 type RegisterParams = {
-  birthday: string,
+  birthday: Date,
   email: string,
   name: string,
   password: string,
@@ -13,81 +15,27 @@ type RegisterParams = {
   username: string
 }
 
-class validateField {
-  static email(email: string): Error | void {
-    const r = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
-
-    if (!r.test(email)) {
-      return new Error(`O email ${email} não é válido`);
-    }
-  }
-
-  static username(username: string): Error | void {
-    const r = /^[\.\-\w]{3,13}$/
-
-    if (!r.test(username)) {
-      return new Error(`O usuário ${username} não é válido`);
-    }
-  }
-
-  static password(password: string): Error | void {
-    let hasUpper: boolean = false; 
-    let hasLower: boolean = false;
-    let hasNum: boolean = false;
-    let hasSpecialChar: Boolean = false;
-
-    if (password.length < 8) {
-      return new Error(`A senha precisa ser mais comprida`);
-    }
-
-    for (let i = 0; i < password.length; i++) {
-      const char = password[i];
-      switch (true) {
-        case char == char.toUpperCase():
-          hasUpper = true;
-          break;
-      
-        case char == char.toLowerCase():
-          hasLower = true;
-          break;
-        
-        case char >= '0' && char <= '9':
-          hasNum = true;
-          break;
-        
-        case /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(char):
-          hasSpecialChar = true;
-          break;
-      }
-    }
-
-    if (!hasUpper) {
-      return new Error(`Senha precisa ter ao menos uma letras maiúsculas`);
-    }
-  
-    if (!hasLower) {
-      return new Error(`Senha precisa ter ao menos uma letra minúscula`);
-    }
-  
-    if (!hasNum) {
-      return new Error(`Senha precisa ter ao menos um número`);
-    }
-  
-    if (!hasSpecialChar) {
-      return new Error(`Senha precisa ter ao menos um caractere especial`);
-    }
-  }
-}
-
 class FetchIdentities {
   static register(register: RegisterParams) {
-    const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/i;
-    const usernameRegex = /^[\.\-\w]{3,13}$/
+    try {
+      validateField.email(register.email);
+      validateField.noSpecialChar("name", register.name);
+      validateField.birthday(register.birthday);
+      validateField.password(register.password);
+      validateField.username(register.username);
+
+      axios.post(`${process.env.BASEURL}/api/register`, register)
+      .then((response) => {
+        alert(response.data);
+      })
+    } catch(err) {
+      alert(err)
+    }
   }
 }
 
 const ContainerForm: React.FC = () => {
-  const [forms, setForms] = React.useState('')
+  const [forms, setForms] = React.useState('');
   let ComponentForm;
 
   switch (forms) {
@@ -116,11 +64,11 @@ const ContainerForm: React.FC = () => {
   );
 };
 
-interface LoginProps {
+interface navigateFormProps {
   setForms(forms: string): void;
 }
 
-const Login: React.FC<LoginProps> = (props) => {
+const Login: React.FC<navigateFormProps> = (props) => {
   return (
     <div className="forms" id="login">
       <IonInput color="dark" className='field' labelPlacement="floating" fill="solid" label='Username'></IonInput>
@@ -139,10 +87,23 @@ const Login: React.FC<LoginProps> = (props) => {
   )
 }
 
-const Register: React.FC<LoginProps> = (props) => {
+const Register: React.FC<navigateFormProps> = (props) => {
   const Next = (<IonButton className="sendForm" color="secondary" onClick={() => NextOrSend()}>Próximo<IonIcon slot='end' icon={arrowForward} /></IonButton>);
   const [component, setComponent] = React.useState(Next);
   const [previewImage, setPreviewImage] = React.useState('');
+  const [validateEmail, setValidateEmail] = React.useState<Error | "">();
+  const [validatePassword, setValidatePassword] = React.useState<Error | "">();
+  const [validateCopyPassword, setValidateCopyPassword] = React.useState<Error | "">();
+  const [validateName, setValidateName] = React.useState<Error | "">();
+  const [validateUsername, setValidateUsername] = React.useState<Error | "">();
+  const [validateDate, setValidateDate] = React.useState<Error | "">();
+  const [isEmailTouched, setIsEmailTouched] = React.useState(false);
+  const [isPasswordTouched, setIsPasswordTouched] = React.useState(false);
+  const [isCopyPasswordTouched, setIsCopyPasswordTouched] = React.useState(false);
+  const [isNameTouched, setIsNameTouched] = React.useState(false);
+  const [isUsernameTouched, setIsUsernameTouched] = React.useState(false);
+  const [isDateTouched, setIsDateTouched] = React.useState(false);
+
   const NextOrSend = () => {
     const parent = document.getElementById("registerContainer") as HTMLElement;
     const getClasses = document.getElementsByClassName("stepIncomplete");
@@ -221,6 +182,42 @@ const Register: React.FC<LoginProps> = (props) => {
     setPreviewImage(URL.createObjectURL(content.files[0]))
   }
 
+  const validationMask = useMaskito({
+    options: {
+      mask: ['5', '5', '9', ...Array(8).fill(/\d/)],
+    },
+  });
+
+  const insertRefPhoneNum = () => {
+    const validation = document.getElementById("phoneNum") as HTMLInputElement;
+    validationMask(validation);
+  }
+
+  const validate = (
+    ev: Event, 
+    update: React.Dispatch<React.SetStateAction<"" | Error | undefined>>, 
+    func: Function) => 
+  {
+    const value = (ev.target as HTMLInputElement).value;
+    setValidateEmail(undefined);
+    if (value === '') return;
+    const err = func(value);
+    !err ? update("") : update(err);
+  };
+
+  const markTouched = (touch: React.Dispatch<React.SetStateAction<boolean>>) => {
+    touch(true);
+  };
+
+  const samePassword = () => {
+    const pw = document.getElementById("passwordField") as HTMLInputElement;
+    const cpw = document.getElementById("passwordCopyField") as HTMLInputElement;
+
+    if (pw.value != cpw.value) {
+      return new Error("A confirmação da senha tem que ser identica a senha");
+    }
+  }
+
   return (
     <div className="forms" id="login">
       <div id='registerContainer'>
@@ -233,18 +230,86 @@ const Register: React.FC<LoginProps> = (props) => {
             </label>
             <input type="file" accept='img/*' id="insertAvatar" onChange={imagePreview}/>
           </div>
-          <IonInput color="dark" className='field' labelPlacement="floating" fill="solid" label='Username'></IonInput>
+          <IonInput
+            className={`field ${validateUsername === "" && 'ion-valid'} ${validateUsername != "" && 'ion-invalid'} ${isUsernameTouched && 'ion-touched'}`}
+            color="dark" 
+            labelPlacement="floating" 
+            fill="solid" 
+            label='Username'
+            errorText={validateUsername instanceof Error ? validateUsername.message : validateUsername}
+            onIonInput={(event) => validate(event, setValidateUsername, validateField.username)}
+            onIonBlur={() => markTouched(setIsUsernameTouched)}
+          />
         </div>
         <div className='registerStep stepIncomplete'>
-          <IonInput color="dark" className='field' labelPlacement="floating" fill="solid" label='Email' type='email'></IonInput>
-          <IonInput color="dark" className='field' labelPlacement="floating" fill="solid" label='Número Celular' placeholder='912345678'></IonInput>
-          <IonInput color="dark" className='field' labelPlacement="floating" fill="solid" label='Senha' type='password'></IonInput>
-          <IonInput color="dark" className='field' labelPlacement="floating" fill="solid" label='Confirmar senha' type='password'></IonInput>
+          <IonInput 
+            className={`field ${validateEmail === "" && 'ion-valid'} ${validateEmail != "" && 'ion-invalid'} ${isEmailTouched && 'ion-touched'}`} 
+            color="dark"
+            labelPlacement="floating" 
+            fill="solid" 
+            label='Email' 
+            type='email'
+            errorText={validateEmail instanceof Error ? validateEmail.message : validateEmail}
+            onIonInput={(event) => validate(event, setValidateEmail, validateField.email)}
+            onIonBlur={() => markTouched(setIsEmailTouched)}
+          />
+          <IonInput 
+            className='field' 
+            labelPlacement="floating" 
+            fill="solid" 
+            label='Número Celular' 
+            id="phoneNum" 
+            placeholder='55912345678' 
+            color="dark" 
+            onIonInput={insertRefPhoneNum}
+          />
+          <IonInput 
+            className={`field ${validatePassword === "" && 'ion-valid'} ${validatePassword != "" && 'ion-invalid'} ${isPasswordTouched && 'ion-touched'}`} 
+            id="passwordField"
+            color="dark" 
+            labelPlacement="floating" 
+            fill="solid" 
+            label='Senha' 
+            type='password'
+            errorText={validatePassword instanceof Error ? validatePassword.message : validatePassword}
+            onIonInput={(event) => validate(event, setValidatePassword, validateField.password)}
+            onIonBlur={() => markTouched(setIsPasswordTouched)}
+          />
+          <IonInput
+            className={`field ${validateCopyPassword === "" && 'ion-valid'} ${validateCopyPassword != "" && 'ion-invalid'} ${isCopyPasswordTouched && 'ion-touched'}`} 
+            id="passwordCopyField"
+            color="dark"
+            labelPlacement="floating" 
+            fill="solid" 
+            label='Confirmar senha' 
+            type='password'
+            errorText={validateCopyPassword instanceof Error ? validateCopyPassword.message : validateCopyPassword}
+            onIonInput={(event) => validate(event, setValidateCopyPassword, samePassword)}
+            onIonBlur={() => markTouched(setIsCopyPasswordTouched)}
+          />
         </div>
         <div className='registerStep stepIncomplete'>
-          <IonInput color="dark" className='field' labelPlacement="floating" fill="solid" label='Nome Completo'></IonInput>
-          <IonInput color="dark" className='field' labelPlacement="floating" fill="solid" label='Username'></IonInput>
-          <IonInput color="dark" className='field' labelPlacement="floating" fill="solid" label='Data de Nascimento' type='date'></IonInput>
+          <IonInput 
+            className={`field ${validateName === "" && 'ion-valid'} ${validateName != "" && 'ion-invalid'} ${isNameTouched && 'ion-touched'}`}
+            color="dark" 
+            labelPlacement="floating" 
+            fill="solid" 
+            label='Nome Completo'
+            errorText={validateName instanceof Error ? validateName.message : validateName}
+            onIonInput={(event) => validate(event, setValidateName, validateField.noSpecialChar)}
+            onIonBlur={() => markTouched(setIsNameTouched)}
+          />
+          <IonInput
+            className={`field ${validateDate === "" && 'ion-valid'} ${validateDate != "" && 'ion-invalid'} ${isDateTouched && 'ion-touched'}`}
+            color="dark"
+            labelPlacement="floating" 
+            fill="solid" 
+            label='Data de Nascimento' 
+            type='date'
+            errorText={validateDate instanceof Error ? validateDate.message : validateDate}
+            onIonInput={(event) => validate(event, setValidateDate, validateField.birthday)}
+            onIonBlur={() => markTouched(setIsDateTouched)}
+          />
           <IonInput color="dark" className='field' labelPlacement="floating" fill="solid" label='Endereço'></IonInput>
         </div>
       </div>
@@ -258,7 +323,7 @@ const Register: React.FC<LoginProps> = (props) => {
   )
 }
 
-const RecoveryPart1: React.FC<LoginProps> = (props) => {
+const RecoveryPart1: React.FC<navigateFormProps> = (props) => {
   return (
     <div className="forms" id="recoveryPart1">
       <br></br><br></br>
@@ -278,7 +343,7 @@ const RecoveryPart1: React.FC<LoginProps> = (props) => {
   )
 }
 
-const RecoveryPart2: React.FC<LoginProps> = (props) => {
+const RecoveryPart2: React.FC<navigateFormProps> = (props) => {
   const validationMask = useMaskito({
     options: {
       mask: [...Array(6).fill(/\d/)],
